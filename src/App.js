c6e5d19bb89154ff5f80bd187dd55091d32b2008
Caythian <xcatherine020202@gmail.com>
2023-10-07 17:03:36 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -4,20 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div className="headbar">
+        <h className="header">Origami:  <span className="header2">More than What You Think</span></h>
+    </div>
+    {/* <div className="menu-icon" onclick="menupop()"> */}
+        {/* <img src="Images/bars-solid.svg" width="2%" > */}
+    {/* </div> */}
+    <div className="menu" id="menu">
+        <li><a href="HOME.html">Home</a></li>
+        <li><a href="CUL.html">Origami as a Culture</a></li>
+        <li><a href="TUTORIAL.html">Tutorial</a></li>
+        <li><a href="REC.html">Virtual Archive</a></li>
+        
+    </div>
+    <div className="imageholder">
+        {/* <img src="Images/OrigamiIntro.jpeg" alt="OrigamiIntro" width="80%"> */}
+    </div>
+    <div className="Text">
+        <p className="intro">
+            Welcome to <span className="emph">Origami: More than What You Think</span>, where the intricate art of paper folding unfolds. With roots deep in Japan's history, origami is not just a craft but a testament to the country's rich cultural tapestry, intertwined with Shinto rituals and ancient ceremonies. While the past is embroidered with tradition, the world of origami today is vast and diverse, encompassing the lifelike beauty of figurative origami, the collective allure of modular pieces, and the hands-on utility of practical origami. Dive into our expansive resources to journey through its storied past, discover the myriad ways origami graces our daily lives, and delve deep into the fascinating mathematics underpinning each fold and crease. Welcome to a world where art meets precision, and tradition meets innovation. Welcome to the universe of origami.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    </div>
+    <div className="footer">
+        <p>&copy; 2023 Origami: More than What You Think. All Rights Reserved.</p>
+    </div>
+<script src="script.js"></script>        
     </div>
   );
 }
